7dce4fc11e19af15f559d0b3f646ae59a66c37a2
Mohamed Jafrun <128215769+jafrundotpy@users.noreply.github.com>
2026-04-15 17:54:50 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,7 +135,11 @@ async def main():
     await app.run_polling()
 
 
-# ---------- START ----------
+# ---------- start -----------
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
