ddf63ce2735a5edcc8b67a466bdbfc29a3bcb8a1
Mohamed Jafrun <128215769+jafrundotpy@users.noreply.github.com>
2026-04-15 17:00:28 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,4 +132,10 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
